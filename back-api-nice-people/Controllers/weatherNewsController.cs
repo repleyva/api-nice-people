1d2202270081ef2224aae0b2459eaca2cb584c27
repleyva <rusvelpassos@gmail.com>
2021-10-28 21:42:58 -0500

    anexando api en c#

---
+++ back-api-nice-people/Controllers/weatherNewsController.cs
@@ -0,0 +1,47 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace back_api_nice_people.NewFolder
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class weatherNewsController : ControllerBase
+    {
+
+        [HttpGet("{search}")]
+        public string Get(string search)
+        {
+
+            string weather = "https://api.openweathermap.org/data/2.5/weather?q="+search+"&appid=7811643fbf7e9082211e1353c932fab0";
+            string news = "http://api.mediastack.com/v1/news?access_key=7e518e43ad5ebc4f8cee265da95d51b7&keywords="+search;
+            var jsonWeather = new WebClient().DownloadString(weather);
+            var jsonNews = new WebClient().DownloadString(news);
+
+            string json = JsonConvert.SerializeObject(new
+            {
+                results = new List<object>()
+                {
+                    new Result{ weather = jsonWeather},
+                    new News{ news = jsonNews },
+                }
+            }) ; 
+
+            //dynamic m = JsonConvert.DeserializeObject(json);
+            return json;
+        }
+    }
+}
+
+public class Result
+{
+    public string weather { get; set; }
+}
+
+public class News
+{
+    public string news { get; set; }
+}
--- ui-api-nice-people/src/context/AppContext.js
+++ ui-api-nice-people/src/context/AppContext.js
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { helpHttps } from "../helpers/helpHttps";
-import { endpoint_news, endpoint_weather } from "../utils/instance";
+import { endpoint_api } from "../utils/instance";
+/* import { endpoint_news, endpoint_weather } from "../utils/instance"; */
 
 const AppContext = createContext();
 const AppProvider = ({ children }) => {
@@ -9,17 +10,17 @@ const AppProvider = ({ children }) => {
   const [search, setSearch] = useState(null);
   const [loading, setLoading] = useState(false);
   const [online, setOnline] = useState(true);
-  const base_url_weather = endpoint_weather(search);
-  const base_url_news = endpoint_news(search);
+  const base_url = endpoint_api(search);
+  /* const base_url_news = endpoint_news(search); */
 
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
-      const [weatherRes, newsRes] = await Promise.all([
-        helpHttps().get(base_url_weather),
-        helpHttps().get(base_url_news),
-      ]);
+			const request = await helpHttps().get(base_url);
       setLoading(false);
+			const json = JSON.parse(request)
+			const weatherRes = JSON.parse(json.results[0].weather);
+			const newsRes = JSON.parse(json.results[1].news);
       setWeather(weatherRes);
       if (newsRes !== null && newsRes.data.length !== 0) {
         newsRes.data.forEach((el) => {
@@ -41,7 +42,7 @@ const AppProvider = ({ children }) => {
     } else {
       setOnline(false);
     }
-  }, [base_url_weather, base_url_news, search]); // cuando search cambie entonces se ejecuta el efecto
+  }, [base_url, search]); // cuando search cambie entonces se ejecuta el efecto
 
   const handleSearch = (query) => {
     setSearch(query);
